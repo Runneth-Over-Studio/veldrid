c2a70dbc1c6bcb486adcabf05e0be29c0692244e
Aaron Salisbury <aaron.salisbury@live.com>
2026-01-18 23:04:03 -0600

    Move vulkan options to vulkan backend.

--- src/Veldrid/LowLevelRenderer/Core/GraphicsDevice.cs
+++ src/Veldrid/LowLevelRenderer/Core/GraphicsDevice.cs
@@ -909,17 +909,6 @@ public abstract class GraphicsDevice : IDisposable
     }
 
     /// <summary>
-    /// Creates a new <see cref="GraphicsDevice"/> using Vulkan.
-    /// </summary>
-    /// <param name="options">Describes several common properties of the GraphicsDevice.</param>
-    /// <param name="vkOptions">The Vulkan-specific options used to create the device.</param>
-    /// <returns>A new <see cref="GraphicsDevice"/> using the Vulkan API.</returns>
-    public static GraphicsDevice CreateVulkan(GraphicsDeviceOptions options, VulkanDeviceOptions vkOptions)
-    {
-        return new VkGraphicsDevice(options, null, vkOptions);
-    }
-
-    /// <summary>
     /// Creates a new <see cref="GraphicsDevice"/> using Vulkan, with a main Swapchain.
     /// </summary>
     /// <param name="options">Describes several common properties of the GraphicsDevice.</param>
@@ -928,40 +917,5 @@ public abstract class GraphicsDevice : IDisposable
     public static GraphicsDevice CreateVulkan(GraphicsDeviceOptions options, SwapchainDescription swapchainDescription)
     {
         return new VkGraphicsDevice(options, swapchainDescription);
-    }
-
-    /// <summary>
-    /// Creates a new <see cref="GraphicsDevice"/> using Vulkan, with a main Swapchain.
-    /// </summary>
-    /// <param name="options">Describes several common properties of the GraphicsDevice.</param>
-    /// <param name="vkOptions">The Vulkan-specific options used to create the device.</param>
-    /// <param name="swapchainDescription">A description of the main Swapchain to create.</param>
-    /// <returns>A new <see cref="GraphicsDevice"/> using the Vulkan API.</returns>
-    public static GraphicsDevice CreateVulkan(
-        GraphicsDeviceOptions options,
-        SwapchainDescription swapchainDescription,
-        VulkanDeviceOptions vkOptions)
-    {
-        return new VkGraphicsDevice(options, swapchainDescription, vkOptions);
-    }
-
-    /// <summary>
-    /// Creates a new <see cref="GraphicsDevice"/> using Vulkan, with a main Swapchain.
-    /// </summary>
-    /// <param name="options">Describes several common properties of the GraphicsDevice.</param>
-    /// <param name="surfaceSource">The source from which a Vulkan surface can be created.</param>
-    /// <param name="width">The initial width of the window.</param>
-    /// <param name="height">The initial height of the window.</param>
-    /// <returns>A new <see cref="GraphicsDevice"/> using the Vulkan API.</returns>
-    public static GraphicsDevice CreateVulkan(GraphicsDeviceOptions options, VkSurfaceSource surfaceSource, uint width, uint height)
-    {
-        SwapchainDescription scDesc = new SwapchainDescription(
-            surfaceSource.GetSurfaceSource(),
-            width, height,
-            options.SwapchainDepthFormat,
-            options.SyncToVerticalBlank,
-            options.SwapchainSrgbFormat);
-
-        return new VkGraphicsDevice(options, scDesc);
     }
 }
